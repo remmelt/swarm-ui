c5f2996b285610dd00b537dae6b7a7efcdfbecb5
Remmelt Pit <remmelt@info.nl>
2014-12-06 18:15:40 +0100

    systems are go

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ shift
 
 echo "I think the token is $TOKEN"
 
-docker run -e "TOKEN=$TOKEN" -d --publish 80:80 --publish 4243:4243 --volume $(pwd)/app:/usr/share/nginx/html $@ --name swarm-ui swarm-ui
+docker run -e "TOKEN=$TOKEN" -d --publish 80:80 --volume $(pwd)/app:/usr/share/nginx/html $@ --name swarm-ui swarm-ui
